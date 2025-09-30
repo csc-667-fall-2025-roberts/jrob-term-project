208a18da76cf55af7a0095cbd8dd640f20f195b1
John Roberts <roberts.john@gmail.com>
2025-09-29 20:36:37 -0700

    testing prettier

---
+++ src/backend/routes/root.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (request, response) => {
+  response.render("root", { gamesListing: ["a", "b", "c", "etc"] });
+});
+
+export default router;
