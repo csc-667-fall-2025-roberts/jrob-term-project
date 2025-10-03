e6adf6d88549630949403aacaf35a0c6c345c99b
John Roberts <roberts.john@gmail.com>
2025-10-02 18:44:03 -0700

    intro-to-express

--- src/backend/routes/root.ts
+++ src/backend/routes/root.ts
@@ -6,4 +6,8 @@ router.get("/", (request, response) => {
   response.render("root", { gamesListing: ["a", "b", "c", "etc"] });
 });
 
+router.post("/", (request, response) => {
+  response.send("you posted");
+});
+
 export default router;
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,5 +1,6 @@
 import * as path from "path";
 import express from "express";
+import morgan from "morgan";
 import createHttpError from "http-errors";
 
 import rootRoutes from "./routes/root";
@@ -9,6 +10,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(morgan("dev"));
 app.use(express.static(path.join("dist", "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
