cc3a944412e985485afccd239168681a9dbae7e6
John Roberts <roberts.john@gmail.com>
2025-11-06 17:39:15 -0800

    Added socket.io; implemented chat

--- src/backend/routes/chat.ts
+++ src/backend/routes/chat.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import * as messageKeys from "../../shared/chat-keys";
 import { Chat } from "../db";
 
 const router = express.Router();
@@ -10,19 +11,20 @@ router.get("/", async (request, response) => {
   const messages = await Chat.list();
 
   const io = request.app.get("io");
-  io.to(id).emit("chat:global:listing", { messages });
+  io.to(id).emit(messageKeys.CHAT_LISTING(), { messages });
 });
 
 router.post("/", async (request, response) => {
-  response.status(202).send();
+  response.status(202);
 
   const { id, username } = request.session.user!;
+  // @ts-ignore
   const { message } = request.body;
 
   const result = await Chat.create(id, message);
 
   const io = request.app.get("io");
-  io.emit("chat:global:message", { ...result, username });
+  io.emit(messageKeys.CHAT_MESSAGE(), { ...result, username });
 });
 
 export default router;
