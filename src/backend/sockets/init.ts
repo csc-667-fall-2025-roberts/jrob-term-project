d02f1502aacf05e178651f16d5bdd13dfe659fdc
John Roberts <roberts.john@gmail.com>
2025-11-10 20:41:11 -0800

    backend updates for game listing, creation, etc

--- src/backend/sockets/init.ts
+++ src/backend/sockets/init.ts
@@ -1,7 +1,8 @@
 import { Server as HTTPServer } from "http";
 import { Server } from "socket.io";
-import { GLOBAL_ROOM } from "../../shared/chat-keys";
+import { GLOBAL_ROOM } from "../../shared/keys";
 import { sessionMiddleware } from "../config/session";
+import { Games } from "../db";
 import logger from "../lib/logger";
 import { User } from "../types/types";
 
@@ -10,7 +11,7 @@ export const initSockets = (httpServer: HTTPServer) => {
 
   io.engine.use(sessionMiddleware);
 
-  io.on("connection", (socket) => {
+  io.on("connection", async (socket) => {
     // @ts-ignore
     const session = socket.request.session as { id: string; user: User };
 
@@ -18,6 +19,17 @@ export const initSockets = (httpServer: HTTPServer) => {
 
     socket.join(session.id);
     socket.join(GLOBAL_ROOM);
+
+    try {
+      const games = await Games.getByUser(session.user.id);
+
+      games.forEach(({ game_id }) => {
+        socket.join(`game:${game_id}`);
+        logger.info(`user ${session.user.id} added to room game:${game_id}`);
+      });
+    } catch (error: unknown) {
+      logger.error(`Error joining game rooms for user ${session.user.id}`);
+    }
 
     socket.on("close", () => {
       logger.info(`socket for user ${session.user.username} closed`);
--- src/shared/chat-keys.ts
+++
@@ -1,3 +0,0 @@
-export const GLOBAL_ROOM = "room:global";
-export const CHAT_MESSAGE = "chat:message";
-export const CHAT_LISTING = "chat:listing";
