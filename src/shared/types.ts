f2ec12541e21bec98efc6b4b6ae3c9429bc7af52
John Roberts <roberts.john@gmail.com>
2025-11-27 14:27:34 -0800

    fixed types

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -47,14 +47,3 @@ export type Game = {
   player_count?: number;
   players?: GamePlayer[];
 };
-
-// ============ Game Cards ============
-export interface GameCard {
-  id: number;
-  game_id: number;
-  card_id: number;
-  owner_id: number;
-  position: number | null;
-  rank?: string;
-  suit?: string;
-}
